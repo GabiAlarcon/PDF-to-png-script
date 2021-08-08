81ca3db2e0f224880edbb74e126f87382db35f59
GabiAlarcon <88625017+GabiAlarcon@users.noreply.github.com>
2021-08-08 18:43:41 +0200

    Add files via upload

---
+++ cmd_class.py
@@ -0,0 +1,194 @@
+import shutil
+import os
+
+class FoutieveRange(Exception):
+    pass
+# maak schrijf logs naar file
+class CmdUtils:
+    @staticmethod
+    def ask_int(message):
+        while True:
+            try:
+                nummer = int(input(message))
+                return nummer
+            except ValueError:
+                print('Dat is geen integer!')
+                
+    @staticmethod            
+    def ask_page_range():
+        while True:
+            try:
+                begin = CmdUtils.ask_int('Begin pagina: ')
+                eind  = CmdUtils.ask_int('Eind pagina: ')
+                if begin < eind:
+                    return begin, eind
+                elif 0 < eind <= begin:
+                    return begin, begin
+                else:
+                    raise FoutieveRange()
+            except FoutieveRange:
+                print('Foute ingave')
+                
+    @staticmethod       
+    def ask_sufix():
+        while True:
+            sufix = input('Wat is de sufix van het bestaand?: ')
+            if sufix:
+                return '_'+sufix
+            else:
+                print('De sufix is empty!')
+                
+    @staticmethod       
+    def ask_prefix():
+        while True:
+            prefix = input('Wat is de prefix van het bestaand?: ')
+            if prefix:
+                return prefix+'_'
+            else:
+                print('De prefix is empty!')
+                
+    @staticmethod            
+    def clear():
+        cls = os.system('cls')
+        
+    @staticmethod
+    def print_files(files):
+        for i,file in enumerate(files):
+            print(f'{i+1}-' ,file)
+
+    @staticmethod
+    def get_all_files():
+        return os.listdir()
+    
+    @staticmethod
+    def get_file_name(file):
+        file, extention = os.path.splitext(file)
+        return file
+    
+    @staticmethod
+    def choose_file(files, num):
+        try:
+            index = int(num)
+            return files[num]
+        except ValueError:
+            print('give_file_name: geen index gegeben (ValueError)')
+
+    @staticmethod
+    def set_file_in_map(file_name, output_map = ''):
+        #output_verwerkte_files(file_name)
+
+        source = os.getcwd() +'\\' +file_name
+        print('source', source)
+        if not output_map:
+            output_map = 'bewerkte'
+        CmdUtils.mkdir(output_map)
+        os.chdir(output_map)
+        CmdUtils.move_file(source, os.getcwd())
+        os.chdir('..')
+
+    @staticmethod    
+    def ask_bevestiging(message):
+        print(message)
+        while True:
+            asw = CmdUtils.ask_int('1- yes, 2- no: ')
+            if asw == 1:
+                return True
+            elif asw == 2:
+                return False
+            else:
+                print('Dat is geen optie!')
+
+    @staticmethod            
+    def enkel_pdf_in_list(files):
+        file_namen = []
+        for i, file in enumerate(files):
+            file_name, extention = os.path.splitext(file)
+            if extention == '.pdf':
+                if is_file_verwerkt_geweest(file):
+                    continue
+                else:
+                    file_namen.append(file)
+                    #print(file, 'wordt verwerkt')
+            else:
+                continue
+        return file_namen
+
+    @staticmethod
+    def save_verwerkte_file(file, output = ''):     
+        if not output:
+            output = 'verwekte_files.txt'
+        if not output in os.listdir():
+            f = open(output, 'w')
+            f.close()
+            
+        f = open(output, 'a')
+        f.write(file+'\n')
+        f.close()
+        
+    @staticmethod
+    def write_err_log(message, output = 'err_logs.txt'):
+        #write to file
+        if not output in os.listdir():
+            f = open(output, 'w')
+            f.close()
+            
+        f = open(output, 'a')
+        f.write(message+'\n')
+        f.close()
+
+    @staticmethod
+    def is_file_verwerkt_geweest(file, verwerkte_file = 'verwekte_files.txt'):
+        if not verwerkte_file in os.listdir():
+            f = open(verwerkte_file,'w')
+            f.close()
+            
+        f = open(verwerkte_file, 'r')
+        lines = f.readlines()   
+        for i, line in enumerate(lines):
+            lines[i] = line.rstrip("\n")
+            
+        if file in lines:
+            return True
+        else:
+            return False
+
+    @staticmethod        
+    def mkdir(path):
+        """Make director
+        Parameters
+        ----------
+        path : str
+        
+        """
+        if not os.path.exists(path):
+            os.makedirs(path)
+    
+    @staticmethod
+    def chdir(path):
+        if not os.path.exists(path):
+            print('Dat map bestaat niet!')
+            return None
+        os.chdir(path)
+        
+
+    @staticmethod
+    def get_files_with_extention(extention, path = ''):
+        files = []
+        if path:
+            os.chdir(path)
+        if not '.' in extention:
+            extention = f'.{extention}'
+            
+        for file in os.listdir():
+            f, ext = os.path.splitext(file)
+            if not ext == extention:
+                continue
+            files.append(file)
+
+        return files
+
+    @staticmethod
+    def move_file(source, destination):
+        #beweegt files naar de dest map
+        #dit om bewerkte bestaanden op te slaan
+        dest = shutil.move(source, destination)
---
+++ take_image.py
@@ -0,0 +1,30 @@
+from pdf2image import convert_from_path
+from cmd_class import CmdUtils as cmd
+source_path =r''
+file_name = 'tekening'
+
+input_file = '{}.pdf'.format(file_name)
+data_source = r'{}{}'.format(source_path, input_file)
+
+#data_source = 
+first_page = 1
+last_page = 1
+
+
+
+output_file_sufix = file_name.replace("extr_", '')
+output_file_sufix = output_file_sufix.replace("Service Manual_", '')
+
+output_path = r'C:\Users\GA\Documents\automation workshop\python\data\images'
+out_dir = '{}\{}'.format(output_path, output_file_sufix)
+cmd.mkdir(out_dir)
+
+
+# Store Pdf with convert_from_path function
+images = convert_from_path(data_source, first_page=first_page, last_page = last_page)
+#print(len(images))
+images[0].save(output_file, 'JPEG')
+for index, image in enumerate(images):
+    output_file = r'{}\{}_pag-{}.jpg'.format(out_dir, output_file_sufix, (index+first_page))
+    # Save pages as images in the pdf
+    image.save(output_file, 'JPEG')
